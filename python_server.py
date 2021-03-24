bc544d7f25e3d52c96c6e038434231fd3d14bd40
mandicLuka <luka.mandic311@gmail.com>
2021-03-24 08:45:50 +0100

    Add primitive python server

---
+++ python_server.py
@@ -0,0 +1,50 @@
+from http.server import BaseHTTPRequestHandler, HTTPServer
+import json
+import cgi
+
+class Server(BaseHTTPRequestHandler):
+    def _set_headers(self):
+        self.send_response(200)
+        self.send_header('Content-type', 'application/json')
+        self.end_headers()
+        
+    def do_GET(self):
+        self._set_headers()
+        self.wfile.write(str.encode(
+            json.dumps({'test': 'TEST', 'test2': 'TEST2'}))
+        )
+        
+    def do_POST(self):
+        ctype, _ = cgi.parse_header(self.headers['content-type'])
+        
+        # refuse to receive non-json content
+        if ctype != 'application/json':
+            self.send_response(400)
+            self.end_headers()
+            return
+            
+        # read the message and convert it into a python dictionary
+        length = int(self.headers['content-length'])
+        message = json.loads(self.rfile.read(length))
+        
+        # add a property to the object, just to mess with data
+        message['received'] = 'ok'
+        
+        # send the message back
+        self._set_headers()
+        self.wfile.write(str.encode(json.dumps(message)))
+        
+def run(port=12345):
+    server_address = ('', port)
+    httpd = HTTPServer(server_address, Server)
+    
+    print ('Starting httpd on port %d...' % port)
+    httpd.serve_forever()
+    
+if __name__ == "__main__":
+    from sys import argv
+    
+    if len(argv) == 2:
+        run(port=int(argv[1]))
+    else:
+        run()
